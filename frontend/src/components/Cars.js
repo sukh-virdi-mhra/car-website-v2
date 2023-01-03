0980079539f0c7ce7d6f0bedb41d1fbc996b25f2
SukhVirdiADA <sukh.virdi2@mhra.gov.uk>
2023-01-03 17:07:05 +0000

    start making the car details info return on same page

--- frontend/src/components/Cars.js
+++ frontend/src/components/Cars.js
@@ -3,6 +3,7 @@ import "../styles/cars.css";
 
 function Cars(props) {
   const [localData, setLocalData] = useState([{}]);
+  const [selectedCar, setSelectedCar] = useState(null);
 
   useEffect(() => {
     fetch("/api")
@@ -17,6 +18,10 @@ function Cars(props) {
     currency: "GBP",
   });
 
+  const handleCardClick = (car) => {
+    setSelectedCar(car);
+  };
+
   return (
     <div className="grid-container">
       <h1 className="center-title">Favourite Cars</h1>
@@ -25,7 +30,7 @@ function Cars(props) {
       ) : (
         <div className="grid">
           {localData.cars.map((car, i) => (
-            <div key={i} className="card">
+            <div key={i} className="card" onClick={() => handleCardClick(car)}>
               <img
                 src={car.image}
                 alt={car.name}
@@ -38,6 +43,27 @@ function Cars(props) {
           ))}
         </div>
       )}
+      {selectedCar ? <CarDetails car={selectedCar} /> : null}
+    </div>
+  );
+}
+
+function CarDetails(props) {
+  const { car } = props;
+  const formatter = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  });
+
+  return (
+    <div>
+      <h1>{car.name}</h1>
+      <img src={car.image} alt={car.name} style={{ width: "100%" }} />
+      <p>{car.summary}</p>
+      <p>{car.description}</p>
+      <p>Price: {car.price ? formatter.format(car.price) : <span>POA</span>}</p>
+      <p>Mileage: {car.mileage}</p>
+      <p>Year: {car.year}</p>
     </div>
   );
 }
