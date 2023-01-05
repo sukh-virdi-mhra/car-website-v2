6ce79d2660c1e8ab8fce323d931158205e772993
SukhVirdiADA <sukh.virdi2@mhra.gov.uk>
2023-01-05 21:46:24 +0000

    fix mobile styling bug

--- frontend/src/components/Cars.js
+++ frontend/src/components/Cars.js
@@ -39,47 +39,53 @@ function Cars(props) {
   return (
     <div className="cars">
       <br />{" "}
-      <h2 className="homepage__header">
-        Current Inventory as of{" "}
-        <span>
-          {`(${date.toLocaleDateString("en-GB", {
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-          })})`}
-        </span>
-      </h2>
-      <p className="homepage__description">
-        This page contains our current supercars for sale where you can view all
-        of the vehicles available in our showroom. From sleek sports cars to
-        powerful luxury vehicles, we have a wide selection of supercars that are
-        sure to satisfy any car enthusiast. Take some time to browse through our
-        inventory and see all of the incredible options we have available.
-        Whether you're looking for a car to turn heads on the open road or one
-        to add to your collection, we have something for everyone. So come on in
-        and take a look at our impressive selection of supercars - we're sure
-        you'll find something you love!
-        <br />
-        <br />
-      </p>
-      {typeof localData.cars === "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="grid">
-          {localData.cars.map((car, i) => (
-            <div key={i} className="card" onClick={() => handleCardClick(car)}>
-              <img
-                src={car.image}
-                alt={car.name}
-                style={{ width: "100%", height: "200px" }}
-              />
-              <h3>{car.name}</h3>
-              {car.price ? <p>{formatter.format(car.price)}</p> : <p>POA</p>}
-            </div>
-          ))}
-        </div>
-      )}
-      {selectedCar ? <CarDetails car={selectedCar} /> : null}
+      <div className="homepage__content">
+        <h2 className="homepage__header">
+          Current Inventory as of{" "}
+          <span>
+            {`(${date.toLocaleDateString("en-GB", {
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })})`}
+          </span>
+        </h2>
+        <p className="homepage__description">
+          This page contains our current supercars for sale where you can view
+          all of the vehicles available in our showroom. From sleek sports cars
+          to powerful luxury vehicles, we have a wide selection of supercars
+          that are sure to satisfy any car enthusiast. Take some time to browse
+          through our inventory and see all of the incredible options we have
+          available. Whether you're looking for a car to turn heads on the open
+          road or one to add to your collection, we have something for everyone.
+          So come on in and take a look at our impressive selection of supercars
+          - we're sure you'll find something you love!
+          <br />
+          <br />
+        </p>
+        {typeof localData.cars === "undefined" ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="grid">
+            {localData.cars.map((car, i) => (
+              <div
+                key={i}
+                className="card"
+                onClick={() => handleCardClick(car)}
+              >
+                <img
+                  src={car.image}
+                  alt={car.name}
+                  style={{ width: "100%", height: "200px" }}
+                />
+                <h3>{car.name}</h3>
+                {car.price ? <p>{formatter.format(car.price)}</p> : <p>POA</p>}
+              </div>
+            ))}
+          </div>
+        )}
+        {selectedCar ? <CarDetails car={selectedCar} /> : null}
+      </div>
     </div>
   );
 }
