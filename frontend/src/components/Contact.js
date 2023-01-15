b6eef43bde48763adfc9ce0cbc6c9883a66c0984
SukhVirdiADA <sukh.virdi2@mhra.gov.uk>
2023-01-15 16:34:18 +0000

    handle form submission and send email using fetch

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/styles.css";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    car: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { firstName, lastName, email, phone, car } = formData;
+    fetch("/send-email", {
+      method: "POST",
+      body: JSON.stringify({ firstName, lastName, email, phone, car }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("Success:", JSON.stringify(response));
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
     <div className="homepage">
       <div className="homepage__content">
@@ -14,28 +37,68 @@ const Contact = () => {
           with more information. Thank you for choosing Veloce Virdi as your
           go-to dealership for all your supercar needs.
         </p>
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={handleSubmit}>
           <div style={{ display: "flex" }}>
             <div style={{ width: "50%", marginRight: "10px" }}>
               <label htmlFor="first-name">First Name</label>
-              <input type="text" id="first-name" name="first-name" required />
+              <input
+                type="text"
+                id="first-name"
+                name="first-name"
+                required
+                value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
+              />
             </div>
             <div style={{ width: "50%" }}>
               <label htmlFor="last-name">Last Name</label>
-              <input type="text" id="last-name" name="last-name" required />
+              <input
+                type="text"
+                id="last-name"
+                name="last-name"
+                required
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
+              />
             </div>
           </div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            required
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
           <br />
           <label htmlFor="phone">Phone</label>
-          <input type="text" id="phone" name="phone" required />
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            required
+            value={formData.phone}
+            onChange={(e) =>
+              setFormData({ ...formData, phone: e.target.value })
+            }
+          />
           <br />
-          <label htmlFor="car-make">Car Make</label>
-          <input type="text" id="car-make" name="car-make" required />
-          <br />
-          <label htmlFor="car-model">Car Model</label>
-          <input type="text" id="car-model" name="car-model" required />
+          <label htmlFor="car">Car</label>
+          <input
+            type="text"
+            id="car"
+            name="car"
+            required
+            value={formData.car}
+            onChange={(e) => setFormData({ ...formData, car: e.target.value })}
+          />
           <br />
           <br />
           <button type="submit">Submit</button>
