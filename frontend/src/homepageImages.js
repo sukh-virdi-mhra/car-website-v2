de88b80677906c9ecf6a1248a483a6ee3ffd0430
“sukh-virdi-mhra” <sukh.virdi2@mhra.gov.uk>
2024-07-21 21:39:11 +0100

    create homepageimage objects

---
+++ frontend/src/homepageImages.js
@@ -0,0 +1,24 @@
+const homepageImages = [
+  {
+    src: "https://www.joemacari.com/images/slider/swiper/1.jpg",
+    alt: "Ferrari Testarossa",
+  },
+  {
+    src: "https://www.joemacari.com/images/slider/swiper/2.jpg",
+    alt: "Porsche 918",
+  },
+  {
+    src: "https://www.joemacari.com/images/slider/swiper/3.jpg",
+    alt: "LaFerrari Ferrari F50",
+  },
+  {
+    src: "https://www.joemacari.com/images/slider/swiper/4.jpg",
+    alt: "Porsche 993 Turbo GT",
+  },
+  {
+    src: "https://www.joemacari.com/images/slider/swiper/5.jpg",
+    alt: "Ferrari Mondial",
+  },
+];
+
+module.exports = homepageImages;
